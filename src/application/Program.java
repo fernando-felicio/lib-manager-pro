f99de538b1ab96fd88a293d41d89f762c4585895
Fernando Felício <fernandofelicio.dev@gmail.com>
2024-01-19 19:09:03 -0300

    Implementing the main program

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,31 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+import entities.Category;
+import entities.Book;
+import entities.enums.BookStatus;
+
 public class Program {
+	
+	public static void main (String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("How many books would you like to add? ");
+		int n = scanner.nextInt();
+		
+		for (int i = 0; i < n; i++) {
+			
+		}
+		
+		
+		
+		
+		scanner.close();
+		
+		
+	}
 
 }
--- src/entities/enums/BookStatus.java
+++ src/entities/enums/BookStatus.java
@@ -2,7 +2,7 @@ package entities.enums;
 
 public enum BookStatus {
 	
-	AVAILABLE,
-	UNAVAILABLE,
+	AVAILABLE, // if a book is borrowed, its status will be displayed in the main program
+	UNAVAILABLE, // if a book is borrowed, its status will be displayed in the main program
 
 }
